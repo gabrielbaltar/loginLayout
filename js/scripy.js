72130e2214829234a65b120ce8bfad05eea81a2b
Gabriel Baltar <gabrielbaltar@Mac-mini-de-Gabriel.local>
2024-03-26 22:01:08 -0300

    Atualização Input

--- js/scripy.js
+++ js/scripy.js
@@ -1,8 +1,9 @@
 const inputName = document.querySelector(".nome-usuario");
 const inputEmail = document.querySelector(".email");
+const inputPassword = document.querySelector(".senha");
 const body = document.querySelector("body");
 
-console.log(inputName);
+console.log(inputPassword);
 
 /* Adicionar a interação ao input de Nome de usuário*/
 function showBorderName(event){
@@ -21,19 +22,33 @@ inputName.addEventListener("click", showBorderName);
 function showBorder(event) {
     inputEmail.classList.add("email-border");
     event.stopPropagation();
-    console.log("click");
 }
 
 function retirarBorder() {
     inputEmail.classList.remove("email-border");
-    console.log("click");
+}
+
+/* Adicionar a interação ao input de senha*/
+function showBorderPassword(event){
+    inputPassword.classList.add("senha-border");
+    event.stopPropagation();
+}
+
+function retirarBorderPassword() {
+    inputPassword.classList.remove("senha-border");
 }
 
 /* Adicionar a interação no input de nome de usuário*/
+inputName.addEventListener("click", retirarBorder);
+inputName.addEventListener("click", retirarBorderPassword);
 inputEmail.addEventListener("click", showBorder);
 inputEmail.addEventListener("click", retirarBorderName);
-inputName.addEventListener("click", retirarBorder);
+inputEmail.addEventListener("click", retirarBorderPassword);
+inputPassword.addEventListener("click", showBorderPassword);
+inputPassword.addEventListener("click", retirarBorderName);
+inputPassword.addEventListener("click", retirarBorder);
 body.addEventListener("click", retirarBorder);
 body.addEventListener("click", retirarBorderName);
+body.addEventListener("click", retirarBorderPassword);
 
 
